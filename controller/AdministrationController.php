d8fd6e451dc917cbcc714226deb87f635c2e0672
nico <nico@27af9564-3bc5-dd47-9581-61e4c64fccd6>
2010-12-20 19:58:33 +0000

    git-svn-id: file:///media/ZEN Stone/wombat@67 27af9564-3bc5-dd47-9581-61e4c64fccd6

---
+++ controller/AdministrationController.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Description of AdministratorController
+ *
+ * @author nico
+ */
+class AdministrationController extends BaseController {
+
+    //put your code here
+
+    public function __construct($registry) {
+        parent::__construct($registry);
+    }
+
+    public function init() {
+        
+    }
+
+    public function indexAction() {
+
+    }
+
+    public function setAction() {
+        
+    }
+
+}
+
+?>
