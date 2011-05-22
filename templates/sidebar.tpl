b10df1f5561dc948c29e592a7db608ab7196a809
nico <nico@27af9564-3bc5-dd47-9581-61e4c64fccd6>
2011-05-22 18:36:01 +0000

    CHG: CSS Form Changes
    git-svn-id: file:///media/ZEN Stone/wombat@124 27af9564-3bc5-dd47-9581-61e4c64fccd6

--- templates/sidebar.tpl
+++ templates/sidebar.tpl
@@ -1,2 +1,5 @@
 <img src="images/wombat_logo.png" alt="logo" />
+<ul>
+    <li></li>
+</ul>
 {include file=menu.tpl}
--- templates_c/%%68^689^68964653%%head.tpl.php
+++ templates_c/%%68^689^68964653%%head.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.26, created on 2011-05-22 17:59:48
+<?php /* Smarty version 2.6.26, created on 2011-05-22 20:17:11
          compiled from head.tpl */ ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
@@ -13,15 +13,4 @@
         <meta name="Copyright" content="2010 Nico Schmitz" />
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
         <link rel="stylesheet" href="css/template.css" type="text/css" media="screen" />
-        <link rel="stylesheet" href="css/typography.css" type="text/css" media="screen" />
-        <link rel="stylesheet" href="css/predefined.css" type="text/css" media="screen" />
-        <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" />
-        <link rel="stylesheet" href="css/table.css" type="text/css" media="screen" />
-        <link rel="stylesheet" href="css/menu.css" type="text/css" media="screen" />
-        <link rel="stylesheet" href="css/generic.css" type="text/css" media="screen" />
-        <link rel="stylesheet" href="css/form.css" type="text/css" media="screen" />
-        <link rel="stylesheet" href="css/awesome.css" type="text/css" media="screen" />
-        <link rel="stylesheet" href="css/jquery-ui-1.8.11.custom.css" type="text/css" media="screen" />
-        <link rel="stylesheet" type="text/css" href="library/dhtmlx/dhtmlxGrid/codebase/dhtmlxgrid.css">
-        <link rel="stylesheet" type="text/css" href="library/dhtmlx/dhtmlxGrid/codebase/skins/dhtmlxgrid_dhx_skyblue.css">
     </head>
