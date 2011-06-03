e3288761273f08bf5b3ca3b5e5d80cda46094431
Nico Schmitz <cofilew@gmail.com>
2011-06-04 01:24:49 +0200

    Bootstrap jetzt OOP im Ordner Core zu finden, index.php im Root Verzeichnis ruft die Klasse auf und führt diese aus.
    Signed-off-by: Nico Schmitz <cofilew@gmail.com>

---
+++ core/Bootstrap.php
@@ -0,0 +1,151 @@
+<?php
+
+/**
+ * Description of Bootstrap
+
+ *  * @author  Nico Schmitz - cofilew@gmail.com
+ * @file    Bootstrap.php
+ * @since   03.06.2011 - 23:58:40
+ */
+class Bootstrap {
+
+    public static $registry = null;
+    public static $config = null;
+    public static $url = null;
+    public static $db = null;
+    public static $smarty = null;
+
+    public static function run() {
+        self::prepare();
+    }
+
+    public static function prepare() {
+        // include needed files
+        require_once('library/Zend/File/Transfer.php');
+        self::setupErrorReporting();
+        self::setupAutoloader();
+        self::setupConfiguration();
+        self::setupDatabase();
+        self::setupSmarty();
+        self::setupRegistry();
+        self::setupUrlParser();
+        self::setRegistryObjects();
+        self::setupNavigation();
+        self::assignSmartyVariables();
+        self::setupRouter();
+        self::closeDatabaseConnection();
+    }
+
+    public static function setupAutoloader() {
+        /* call the autoloader */
+        require_once('core/Autoloader.php');
+        Autoloader::init();
+    }
+
+    //Error reporting setting
+    public static function setupErrorReporting() {
+        /* prints out all error messages */
+        error_reporting(E_ALL);
+        self::$smarty->error_reporting = 'E_ALL';
+    }
+
+    //Date time setting will be done here
+    public static function setupDateTime() {
+        date_default_timezone_set('Europe/Berlin');
+    }
+
+    //Registry setting will be done here.
+    public static function setupRegistry() {
+        // create registry object
+        self::$registry = Registry::getInstance();
+    }
+
+    public static function setupSession() {
+        /* Launching Session */
+        session_start();
+        session_set_cookie_params(7200, '', true);
+    }
+
+    //Configuration file reading & setting up configuration will be done using following.
+    public function setupConfiguration() {
+        /* load the configurations */
+        self::$config = Config::getInstance('config.ini');
+    }
+
+    // Template Engine setting will be done here
+    public static function setupSmarty() {
+        /* initalize Smarty */
+        require_once('library/Smarty/Smarty.class.php');
+        self::$smarty = new Smarty();
+    }
+
+    // Databse Setup done here
+    public static function setupDatabase() {
+        require_once('library/Zend/Db/Adapter/Pdo/Mysql.php');
+        try {
+            self::$db = new Zend_Db_Adapter_Pdo_Mysql(array(
+                        'host' => self::$config->get('database.params.host'),
+                        'username' => self::$config->get('database.params.user'),
+                        'password' => self::$config->get('database.params.password'),
+                        'charset' => self::$config->get('database.params.charset'),
+                        'dbname' => self::$config->get('database.params.database')));
+        } catch (Zend_Db_Exception $dbException) {
+            die($dbException);
+        }
+    }
+
+    // Url Parset setup will be done here
+    public static function setupUrlParser() {
+        // parse the url
+        self::$url = new Url();
+
+        try {
+            self::$url->parse();
+        } catch (UrlException $urlException) {
+            die($urlException);
+        }
+    }
+
+    // navigation setup will be done here
+    public static function setupNavigation() {
+        $navi = new Navigation();
+        $data = $navi->fetch();
+        $navi->create($data);
+    }
+
+    // registry objects will be set here
+    public static function setRegistryObjects() {
+        // registers $db and $smarty
+        self::$registry->set('db', self::$db);
+        self::$registry->set('smarty', self::$smarty);
+        self::$registry->set('config', self::$config);
+        self::$registry->set('url', self::$url);
+    }
+
+    // smarty variables will be assigned here
+    public static function assignSmartyVariables() {
+        // assign them to smarty
+        self::$smarty->assign('file', 'bootstrap.php');
+        self::$smarty->assign('maintitle', self::$config->get('html.title'));
+        self::$smarty->assign('basepath', self::$config->get('path.base'));
+        self::$smarty->assign('controller', self::$url->get('controller'));
+    }
+
+    // router setup will be done here
+    public static function setupRouter() {
+        $router = new Router();
+
+        try {
+            $router->run();
+        } catch (RouterException $routerException) {
+            die($routerException->getMessage());
+        }
+    }
+
+    // databse connection will be closed here
+    public static function closeDatabaseConnection() {
+        self::$db->closeConnection();
+    }
+
+}
+
