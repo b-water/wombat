bc13def6da952cc0865678fb97df2e66a257586c
Nico Schmitz <nschmitz1991@gmail.com>
2011-07-20 19:10:16 +0200

    - Kommentare korrigieren, erstellen - Programm / Lizenz Kopf hinzufügen - Aufräumen

--- core/Object.php
+++ core/Object.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * wombat
+ * 
+ * LICENCE
+ * 
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. 
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter to Creative Commons, 
+ * 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+ * 
+ * @name wombat
+ * @author Nico Schmitz - nschmitz1991@gmail.com
+ * @copyright  Copyright (c) 2010-2011 Nico Schmitz (nschmitz1991@gmail.com)
+ * @since 01.04.2010
+ * @version 0.1
+ * @license http://creativecommons.org/licenses/by-nc-nd/3.0/ Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License
+ */
+
 
 /**
  * Description of DomainModel
