2790cdfb437ad9bc21b5b21287a318c53e78584c
Nico Schmitz <nschmitz1991@gmail.com>
2011-07-23 09:27:28 +0200

    Entfernen des schliessenden PHP Tags ?>

--- controller/UserController.php
+++ controller/UserController.php
@@ -57,5 +57,3 @@ class UserController extends BaseController {
     }
 
 }
-
-?>
--- core/ConfigException.php
+++ core/ConfigException.php
@@ -24,6 +24,5 @@
  * @file    ConfigException.php
  * @since   13.05.2011 - 23:35:14
  */
-class ConfigException extends Exception {
-}
-?>
+class ConfigException extends Exception {}
+
