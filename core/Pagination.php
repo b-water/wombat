8f5ec7e7b9015cd724af46e388485e856cd07164
Nico Schmitz <mail@nicoschmitz.eu>
2012-02-04 12:40:25 +0100

    CHG: Optionen werden jetzt über ein Array dem Konstruktur übergeben

--- core/Pagination.php
+++ core/Pagination.php
@@ -1,5 +1,19 @@
 <?php
 
+/**
+ * wombat
+ * 
+ * LICENCE
+ * 
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. 
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter to Creative Commons, 
+ * 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+ * 
+ * @name wombat
+ * @author Nico Schmitz - nschmitz1991@gmail.com
+ * @copyright  Copyright (c) 2010-2011 Nico Schmitz (nschmitz1991@gmail.com)
+ * @license http://creativecommons.org/licenses/by-nc-nd/3.0/ Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License
+ */
 class Pagination {
 
     private $entriesPerPage;
@@ -12,16 +26,29 @@ class Pagination {
     private $previousText = 'Vorherige';
     private $nextText = 'Nächste';
 
-    public function __construct($entriesPerPage = 10, $entriesMax = 0, $currentPage = 1, $pageRange = 5) {
-        $this->entriesMax = $entriesMax;
-        $this->entriesPerPage = $entriesPerPage;
-        $this->currentPage = $currentPage;
-        $this->pageRange = $pageRange;
+    /**
+     *
+     * @param array $options
+     * 
+     * Avaible Options :
+     *  
+     * entriesPerPage = maximum entries that are show on a page
+     * entriesMax = the total amount of all entries
+     * currentPage = the current page number
+     * pageRange = how many pages should be shown
+     * previousText = the text from the "previous" link
+     * nextText = the text from the "next" link
+     */
+    public function __construct(array $options) {
+        $this->entriesMax = $options['entriesMax'];
+        $this->entriesPerPage = $options['entriesPerPage'];
+        $this->currentPage = $options['currentPage'];
+        $this->pageRange = $options['pageRange'];
 
         $this->currentEntry = ($this->currentPage - 1) * $this->entriesPerPage;
-        // pages before current page
-        
-        for($i = $this->entriesPerPage;$i >= 1; $i--) {
+// pages before current page
+
+        for ($i = $this->entriesPerPage; $i >= 1; $i--) {
             
         }
         for ($i = $currentPage; $i >= 1; $i--) {
@@ -32,7 +59,7 @@ class Pagination {
             $this->pagesInRange[$i] = $i;
         }
     }
-    
+
     public function getEntriesPerPage() {
         return $this->entriesPerPage;
     }
