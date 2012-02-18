ba3303d9ce07de3a1d08fd8dac8082f9e5dcc819
Nico Schmitz <mail@nicoschmitz.eu>
2012-02-18 23:17:25 +0100

    CHG: Versions und Since Feld aus Kommentar entfernt

--- core/RouterException.php
+++ core/RouterException.php
@@ -11,8 +11,6 @@
  * @name wombat
  * @author Nico Schmitz - nschmitz1991@gmail.com
  * @copyright  Copyright (c) 2010-2011 Nico Schmitz (nschmitz1991@gmail.com)
- * @since 01.04.2010
- * @version 0.1
  * @license http://creativecommons.org/licenses/by-nc-nd/3.0/ Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License
  */
 class RouterException extends Exception {}
