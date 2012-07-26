a32028a359d147cd08e719973bf7783271e65669
Nico Schmitz <mail@nicoschmitz.eu>
2012-07-26 20:36:41 +0200

    CHG: Pagination

--- view/js/core.js
+++ view/js/core.js
@@ -25,30 +25,45 @@ jQuery.fn.exists = function(){
     return jQuery(this).length>0;
 }
 
-$(document).ready(function() {  
+$(document).ready(function() {
+    
+    var defaultDialogConf = {
+        modal : true,
+        width:600,
+        height:800,
+        draggable:false,
+        resizable:false
+    }
+    
+    $('#movie-edit a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    })
+    
+    //    bindUIDialog($('a#add'),'Hinzufügen',defaultDialogConf);
     core.init.form($('form#edit'));
-//    core.init.tooltip($('a'));
+    //    core.init.tooltip($('a'));
     //    core.init.fancybox($('.fancybox'));
     //genre init
     core.genre.autocomplete($('#genre'),'genre/autocomplete/',$('div#assocGenres'));
     core.genre.tokenDelete($('span.genre'));
     //table init
-//    core.table.addEven($('table tr:even'));
-//    core.table.addFancyDelete($('a.fancydelete'));
-//    core.table.sort($('table'));
-//    $('#file').customFileInput({
-//        button_position : 'right',
-//        feedback_text : 'Keine Datei ausgewählt!',
-//        button_text : 'Durchsuchen',
-//        button_change_text : 'Durchsuchen'
-//    });
+    //    core.table.addEven($('table tr:even'));
+    //    core.table.addFancyDelete($('a.fancydelete'));
+    //    core.table.sort($('table'));
+    //    $('#file').customFileInput({
+    //        button_position : 'right',
+    //        feedback_text : 'Keine Datei ausgewählt!',
+    //        button_text : 'Durchsuchen',
+    //        button_change_text : 'Durchsuchen'
+    //    });
     //jquery plugins
     //    $('table#movies').tablesorter();
-    $('.nav-config').click(function(evt) {
-        evt.preventDefault();
-        $('.dropdown-config').toggle('slow',function() {});
-    });
-    
+    //    $('.nav-config').click(function(evt) {
+    //        evt.preventDefault();
+    //        $('.dropdown-config').toggle('slow',function() {});
+    //    });
+    //    
     $('.tip').popover();
 //    if($('table.tablesorter').exists()) {
 //        $('table.tablesorter').tablesorter({
@@ -81,8 +96,36 @@ $(document).ready(function() {
     
 });
 
+function bindUIDialog(obj,title,conf) {
+    if(obj.exists()) {
+        if(title.length > 0) {
+            var dialogHTML = '<div title="'+title+'"></div>';
+        } else {
+            var dialogHTML = '<div></div>';
+        }
+        obj.on('click',function(event) {
+            showUIDialogProgress();
+            event.preventDefault();
+            var dialog = $(dialogHTML).appendTo('body');
+            $.ajax({
+                url: $(this).attr('href'),
+                success : function(data) {
+                    dialog.append(data);
+                    hideUIDialogProgress();
+                    dialog.dialog(conf); 
+                }
+            });
+        });
+    }
+}
 
-    
+function showUIDialogProgress() {
+    $('div#progress').fadeIn('slow');
+}
+
+function hideUIDialogProgress() {
+    $('div#progress').fadeOut('slow');
+}
 
 
 /**
@@ -97,9 +140,9 @@ var core = {
                 showResponse = function() {
                     $('.notice').show();
                             
-//                    setTimeout(function(){
-//                        core.http.redirectToUrl(window.location.pathname);
-//                    }, 2000);
+                //                    setTimeout(function(){
+                //                        core.http.redirectToUrl(window.location.pathname);
+                //                    }, 2000);
                 }
                 var options = {
                     success: showResponse
@@ -126,42 +169,11 @@ var core = {
         }
     },
     ajax : {
+        modal : function(obj, src) {
+            
+        },
         request : function() {
         
-        },
-        initFancyBox : function(id,controller,action, title) {
-        
-            $.fancybox.showActivity();
-        
-            var path = '';
-        
-            if(controller != undefined)
-                path = controller+'/';
-            if(action != undefined)
-                path += action+'/';
-            if(id != undefined)
-                path += id+'';
-        
-            $.ajax({
-                type : 'POST',
-                cache : false,
-                url : path,
-                data : $(this).serializeArray(),
-                success: function(data) {
-                    $.fancybox({
-                        content: data,
-                        title: title,
-                        scrolling: 'no',
-                        onComplete: function() {
-                            $('#fancybox-title').css({
-                                'top':'0px',
-                                'bottom':'auto'
-                            });
-                        }
-                    });
-        
-                }
-            });
         },
         ajaxForm: function() {
             $('form#edit').ajaxForm(function() {});
@@ -224,7 +236,7 @@ var core = {
                     source: src,
                     minLength: 1,
                     select: function(event, ui) {
-                        appObj.prepend('<span class="genre label success"><input type="hidden" name="genre[]" value="'+ui.item.value+'" /><span class="text">'+ui.item.label+'</span><span class="delete">L&ouml;schen</span></span>');
+                        appObj.prepend('<span class="badge badge-success genre label"><input type="hidden" name="genre[]" value="'+ui.item.value+'" /><span class="text">'+ui.item.label+'</span><i class="icon-remove icon-white"></i></span>');
                     },
                     close : function() {
                         obj.val('');
