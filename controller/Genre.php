7c6c013738a393bbfa4e736447cf72329c8804ef
nicoschmitz <nicoschmitz@outlook.de>
2015-03-14 16:36:32 +0100

    Update Genre.php
    remove license header

--- controller/Genre.php
+++ controller/Genre.php
@@ -1,21 +1,5 @@
 <?php
 
-/**
- * wombat
- *
- * LICENCE
- *
- * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
- * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter to Creative Commons,
- * 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
- *
- * @name wombat
- * @author Nico Schmitz - mail@nicoschmitz.eu
- * @copyright  Copyright (c) 2010-2011 Nico Schmitz
- * @since 01.04.2010
- * @version 0.1
- * @license http://creativecommons.org/licenses/by-nc-nd/3.0/ Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License
- */
 require_once('core/Controller.php');
 
 class GenreController extends Controller {
