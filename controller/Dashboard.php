8b14a1c466f6f41b96cf144809fed232c2ab0bec
nicoschmitz <nicoschmitz@outlook.de>
2015-03-14 16:34:48 +0100

    Update Dashboard.php
    remove license header

--- controller/Dashboard.php
+++ controller/Dashboard.php
@@ -1,19 +1,5 @@
 <?php
 
-/**
- * wombat
- * 
- * LICENCE
- * 
- * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. 
- * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter to Creative Commons, 
- * 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
- * 
- * @name wombat
- * @author Nico Schmitz - mail@nicoschmitz.eu
- * @copyright  Copyright (c) 2010-2011 Nico Schmitz
- * @license http://creativecommons.org/licenses/by-nc-nd/3.0/ Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License
- */
 require_once('core/Controller.php');
 
 class DashboardController extends Controller {
