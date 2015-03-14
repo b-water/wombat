ab278835952f19dada4807863bc5607c66ca68c8
nicoschmitz <nicoschmitz@outlook.de>
2015-03-14 16:42:07 +0100

    Update ConfigException.php
    remove license header

--- core/ConfigException.php
+++ core/ConfigException.php
@@ -1,28 +1,4 @@
 <?php
 
-/**
- * wombat
- * 
- * LICENCE
- * 
- * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. 
- * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter to Creative Commons, 
- * 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
- * 
- * @name wombat
- * @author Nico Schmitz - nschmitz1991@gmail.com
- * @copyright  Copyright (c) 2010-2011 Nico Schmitz (nschmitz1991@gmail.com)
- * @since 01.04.2010
- * @version 0.1
- * @license http://creativecommons.org/licenses/by-nc-nd/3.0/ Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License
- */
-
-/**
- * Description of ConfigException
- *
- * @author  Nico Schmitz - nschmitz1991@gmail.com
- * @file    ConfigException.php
- * @since   13.05.2011 - 23:35:14
- */
 class ConfigException extends Exception {}
 
