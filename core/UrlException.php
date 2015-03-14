cde6ef2f49713fd9af1c0d13e16bdac5593e00b0
nicoschmitz <nicoschmitz@outlook.de>
2015-03-14 16:47:43 +0100

    Update UrlException.php
    remove license header

--- core/UrlException.php
+++ core/UrlException.php
@@ -1,20 +1,3 @@
 <?php
 
-/**
- * wombat
- * 
- * LICENCE
- * 
- * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. 
- * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter to Creative Commons, 
- * 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
- * 
- * @name wombat
- * @author Nico Schmitz - mail@nicoschmitz.eu
- * @copyright  Copyright (c) 2010-2011 Nico Schmitz
- * @since   13.05.2011 - 23:35:14
- * @version 0.1
- * @license http://creativecommons.org/licenses/by-nc-nd/3.0/ Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License
- */
-
 class UrlException extends Exception {}
